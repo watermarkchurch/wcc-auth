f4442152f9e856af32c904855175bcbebfc232f7
Travis Petticrew <travis@petticrew.net>
2013-09-30 16:17:04 -0500

    Extracted AccessLevel from Nucleus

---
+++ lib/wcc/auth/access_level.rb
@@ -0,0 +1,38 @@
+module WCC::Auth
+
+  ACCESS_LEVELS = [
+    { id: nil, name: "None", slug: "none", description: "no access", level: 0 },
+    { id: 1, name: "Basic", slug: "basic", description: "read access", level: 1 },
+    { id: 2, name: "Contribute", slug: "contribute", description: "read-write of data user owns", level: 2 },
+    { id: 3, name: "Manage", slug: "manage", description: "read-write of other's data", level: 3 },
+    { id: 4, name: "App Admin", slug: "appadmin", description: "read-write app configuration", level: 4 },
+    { id: 5, name: "System Admin", slug: "sysadmin", description: "full access", level: 5 },
+  ]
+
+  AccessLevel = Struct.new(:id, :name, :slug, :description, :level) do
+    include Comparable
+
+    def <=>(record)
+      self.level <=> record.level
+    end
+
+    def self.[](index_or_slug, db=ACCESS_LEVELS)
+      all(db).select do |al|
+        al.level.to_s === index_or_slug.to_s ||
+          al.slug === index_or_slug.to_s
+      end.last
+    end
+
+    def self.all(db=ACCESS_LEVELS)
+      db.collect do |row|
+        new.tap { |access_level|
+          row.each { |field, value|
+            access_level[field] = value
+          }
+        }
+      end.sort
+    end
+
+  end
+
+end
---
+++ spec/wcc/auth/access_level_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe WCC::Auth::AccessLevel do
+  let(:klass) { WCC::Auth::AccessLevel }
+  let(:test_db) do
+    [
+      { id: 1, name: "Foo", slug: "foo", description: "foo", level: 2 },
+      { id: 2, name: "Bar", slug: "bar", description: "bar", level: 1 },
+    ]
+  end
+
+  describe "class indexing operators" do
+    subject { klass }
+
+    it "returns record by slug if symbol or string is passed" do
+      expect(klass["foo", test_db].level).to eq(2)
+      expect(klass[:bar, test_db].level).to eq(1)
+    end
+
+    it "returns record by level if number is passed" do
+      expect(klass[2, test_db].level).to eq(2)
+      expect(klass["1", test_db].level).to eq(1)
+    end
+
+    it "uses ACCESS_LEVELS db by default" do
+      expect(klass[:appadmin].slug).to eq("appadmin")
+    end
+  end
+
+  describe "::all class method" do
+    subject { klass }
+
+    it "returns array of records as listed in db argument" do
+      levels = subject.all(test_db)
+      expect(levels.count).to eq(2)
+      expect(levels.all? { |level| level.kind_of?(klass) }).to be_true
+    end
+
+    it "returns objects in level order" do
+      levels = subject.all(test_db)
+      expect(levels[0].slug).to eq("bar")
+      expect(levels[1].name).to eq("Foo")
+    end
+
+    it "uses ACCESS_LEVELS constant as DB if non provided" do
+      levels = subject.all
+      expect(levels.count).to eq(WCC::Auth::ACCESS_LEVELS.count)
+      expect(levels.last.slug).to eq("sysadmin")
+    end
+  end
+
+  describe "comparability" do
+    it "implements comparable methods" do
+      a = klass.new
+      b = klass.new
+      a.level = 1
+      b.level = 2
+      expect(a < b).to be_true
+      expect(b == a).to be_false
+      expect(b < a).to be_false
+    end
+  end
+
+end
+
