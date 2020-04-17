dc4448b70ac4a9f64a039a41275782626564ee3e
Justin Powell <jpowellcs@gmail.com>
2020-04-17 16:10:56 -0500

    take stab at dynamic authorize_params

--- lib/wcc/auth/devise.rb
+++ lib/wcc/auth/devise.rb
@@ -1,22 +1,35 @@
-require "wcc/auth"
-require "devise"
+# frozen_string_literal: true
+
+require 'wcc/auth'
+require 'devise'
 
 module WCC
   module Auth
     module Devise
-      autoload :WatermarkCallbacksController, 'wcc/auth/devise/watermark_callbacks_controller'
+      autoload :WatermarkCallbacksController,
+               'wcc/auth/devise/watermark_callbacks_controller'
     end
+
+    ConfiguresOAuth = lambda { |env|
+      request = Rack::Request.new(env)
+
+      env['omniauth.strategy'].options.merge!(
+        client_id: WCC::Auth.config.app_id,
+        client_secret: WCC::Auth.config.app_secret
+      )
+      env['omniauth.strategy'].options[:authorize_params].merge!(
+        request.parameters['authorize_params'] || {}
+      )
+    }
   end
 end
 
-WCC::Auth.finalize_callbacks << -> {
-  require "omniauth/strategies/watermark"
+WCC::Auth.finalize_callbacks << lambda {
+  require 'omniauth/strategies/watermark'
+
   Devise.setup do |config|
-    config.omniauth :watermark,
-      WCC::Auth.config.app_id,
-      WCC::Auth.config.app_secret
+    config.omniauth :watermark, setup: WCC::Auth::ConfiguresAuth
   end
 
   OmniAuth.config.full_host = WCC::Auth.config.app_url
 }
-
